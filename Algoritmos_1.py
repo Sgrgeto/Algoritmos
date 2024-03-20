8f2ad3ca3a908702fa3227c0b6ed11d12679b688
Sgrgeto <120543710+Sgrgeto@users.noreply.github.com>
2024-03-20 08:12:31 -0600

    Update Algoritmos_1.py
    Add mergesort, ternary search

--- Algoritmos_1.py
+++ Algoritmos_1.py
@@ -8,9 +8,36 @@ def sort_array(array):
 			array[j-1],array[j] = array[j], array[j-1]
 			j-=1
 	return array
-def merge_sort(array):
-	if array.size < 2:
-		raise ValueError('Array must have at least 2 values')
+def merge(array:list)->list:
+    left = []
+    right = []
+    if(len(array)==1):
+        return array
+	#Dividimos el arreglo a la mitad
+    middle = int(len(array)/2)
+    left = array[:middle]
+    right = array[middle:]
+    divided_array_l=merge(left)
+    dividev_array_r=merge(right)
+    return mergesort(divided_array_l,dividev_array_r)
+
+def mergesort(left:list,right:list)->list:
+    result = []
+	#Pasamos el arreglo dividido, primero cuando estan solos, despues cuando se hacen pareja, y ps poco a poco los juntamos, y vamos decidiendo cual es mayor
+    while len(left)>0 and len(right)>0:
+        if left[0]>right[0]:
+            result.append(right[0])
+            right.pop(0)
+        else:
+            result.append(left[0])
+            left.pop(0)
+    while len(left)>0:
+        result.append(left[0])
+        left.pop(0)
+    while len(right)>0:
+        result.append(right[0])
+        right.pop(0)
+    return result
 
 def cuenta_numero(numero):
 	if numero == 0:
@@ -95,4 +122,38 @@ def search():
 				return binary_search(lista, number,low = mid+1, high=high)
 	position = binary_search(lista,number, low, high)
 	return position
-
+def terneria(lim_inf:int,lim_sup:int,array:list):
+	'''ordenamos el arreglo'''
+	array.sort()
+	'''el numero a buscar en el arreglo'''                       
+	num_buscar = 6
+	'''estas tres líneas las cheque en un codigo para obtener las particiones'''                      
+	size = lim_sup-lim_inf 
+	'''este es el primer tercio'''             
+	left = int(lim_inf+(size/3)) 
+	'''este es el tercer tercio, '''
+	right = int(lim_inf+((size*2)/3))   
+	'''El tercer tercio es el de enmedio y abarca desde[left:right]'''
+	if(lim_inf<=lim_sup):
+		'''Si el lim_inf es mayor entonces no encontramos el numero'''
+		if(array[left] == num_buscar):  
+			''' Checamos si el numero es igual'''
+			if (array[left-1]==num_buscar):  
+				lim_inf-=1
+	#CASO ESPECIAL CHECAMOS SI EL NUMERO ANTERIOR NO ES EL DEL MISMO ARREGLAR, PARA ASI ENCONTRAR LA PRIMERA POSICION
+				lim_sup=left-1
+				return terneria(lim_inf, lim_sup, array)
+			else:
+				#RETORNAMOS LA POSICION, EL NUMERO A BUSCAR, Y EL ANTERIOR A ESE NUMERO
+			    return left+1,array[left+1],array[left-1]
+		elif(num_buscar< array[left]):
+			#EL ARREGLO AHORA VA A SER DE LA PRIMERA POSICION HASTA EL SEGUNDO TERCIO
+			lim_sup = left-1
+			
+			return terneria(lim_inf,lim_sup,array)
+		else:
+			#SI ES MAYOR EL ARREGLO VA A EMPEZAR DEL SEGUDO TERCIO HASTA EL FINAL DEL ARREGLO
+			lim_inf=right+1
+			return terneria(lim_inf,lim_sup,array)
+	else:
+		return -1#Si no encontramos el numero
